2916cacad2886dd331982fb433721f13c055a1bc
hulyio <00039718@uca.edu.sv>
2021-05-11 23:46:00 -0600

    added iteration to exercise 1

--- Tarea Laboratorio 3/ejercicio1.asm
+++ Tarea Laboratorio 3/ejercicio1.asm
@@ -2,19 +2,25 @@ org 100h
 
 section .text
 
-    XOR AX, AX ; Limpiar el registro
-    XOR BX, BX ; Limpiar el registro
-    XOR CX, CX ; Limpiar el registro
-    ADD BL, 0d ; Sumar Digitos
-    ADD BL, 0d ; Sumar Digitos
-    ADD BL, 0d ; Sumar Digitos
-    ADD BL, 3d ; Sumar Digitos
-    ADD BL, 9d ; Sumar Digitos
-    ADD BL, 7d ; Sumar Digitos
-    ADD BL, 1d ; Sumar Digitos
-    ADD BL, 8d ; Sumar Digitos
-    MOV AL, BL ; Copiar Suma a Dividendo
-    MOV CL, 8d ; Escribir 8 al Divisor
-    DIV CL ; Dividir AX/CL
-    MOV byte [20Ah], AL ; Copiar Cociente AL a 20Ah checar con -d 20A
-    int 20h ; Fin
+    XOR CX, CX
+    XOR AX, AX
+    MOV word CX, 8d
+    CALL sumar
+    CALL promedio
+    CALL fin
+
+sumar:
+    ADD AL, [carnet+DI]
+    INC DI
+    LOOP sumar
+
+promedio:
+    MOV BX, 8d
+    DIV BX
+    MOV [20Ah], AL
+
+fin:
+    int 20h
+
+section .data
+    carnet DB 0,0,0,3,9,7,1,8
